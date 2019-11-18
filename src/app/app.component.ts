d1b03b811081f59b2f964330dab2efe5bf5acf22
Ronal <ronalruizdev@ug.edu.ec>
2019-11-17 20:00:41 -0500

    feat:final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import {resultList, SpeechRecognitionService} from '@kamiazya/ngx-speech-recognition';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    SpeechRecognitionService,
+  ],
 })
 export class AppComponent {
+  private synth = window.speechSynthesis;
+  message = '';
+  private voices;
+  listen = ""
   title = 'ngRaspberryPi';
+  leOn = false
+
+  constructor(public _speech: SpeechRecognitionService){
+
+  }
+
+  commandVoice() {
+    if (this.synth !== null) {
+      this.voices = this.synth.getVoices();
+      const utterThis = new SpeechSynthesisUtterance('Que accion desea realizar');
+      utterThis.lang = 'es-EC';
+      this.synth.speak(utterThis);
+      const speking = this.synth.speaking;
+      this.listen = 'warn';
+      setTimeout(() => {
+        this._speech.start();
+      }, 1500);
+      this._speech.onresult = ({results}) => {
+        this.message = results.item(0).item(0).transcript;
+      };
+      this._speech.onend = () => {
+        this.listen = '';
+        console.log(this.message)
+        if (this.message === 'encender') {
+          this.leOn = true;
+        } else {
+          this.leOn = false;
+        }
+      };
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SpeechRecognitionModule} from '@kamiazya/ngx-speech-recognition';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SpeechRecognitionModule.withConfig({
+      lang: 'es-EC',
+      interimResults: true,
+      maxAlternatives: 10,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
